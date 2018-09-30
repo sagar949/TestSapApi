345fb484d4a386c2bf5e93be180225c224c230e7
sagar949 <vidya.pamula@gmail.com>
2018-09-30 23:29:46 +0530

    Add project files.

---
+++ TestCloudApi/TestCloud.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Web;
+using System.Net.Http.Headers;
+
+namespace TestCloudApi
+{
+    public class TestCloud
+    {
+        public static async Task<HttpResponseMessage> MakeRequest()
+        {
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            // Request headers
+            client.DefaultRequestHeaders.Add("CallerId", "");
+            client.DefaultRequestHeaders.Add("api-version", "1.0.0");
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "04983169bd3747e8baddc7dc1519129b");
+
+            var uri = "https://sapis.azure-api.net/api/getactions?" + queryString;
+
+            HttpResponseMessage response;
+
+            // Request body
+            byte[] byteData = Encoding.UTF8.GetBytes(System.IO.File.ReadAllText(@"D:\Test\TestCloudApi\TestCloudApi\body.txt"));
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                response = await client.PostAsync(uri, content);
+                return response;
+            }
+
+        }
+    }
+}
---
+++ TestCloudApiConsole/Module1.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Net.Http
+Imports TestCloudApi
+Imports Newtonsoft.Json
+Imports Newtonsoft.Json.Linq
+
+Module Module1
+
+    Sub Main()
+        GetResponse()
+        Console.ReadLine()
+    End Sub
+
+    Async Sub GetResponse()
+        Dim res As HttpResponseMessage = Await TestCloud.MakeRequest()
+        Dim responseContent = Await res.Content.ReadAsStringAsync()
+        Console.WriteLine(JToken.Parse(responseContent).ToString(Formatting.Indented))
+    End Sub
+
+End Module
